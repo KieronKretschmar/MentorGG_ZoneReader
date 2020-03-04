72864c81a9e9119ed3bbdb165afda23f41fab8fe
Kieron <kieron@mentor.gg>
2020-03-04 18:23:20 +0100

    Add getter method for Target.Center to enable drawing targets in webapp

--- ZoneReader/LineUpClasses/Target.cs
+++ ZoneReader/LineUpClasses/Target.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using NetTopologySuite.Geometries;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace ZoneReader.LineUpClasses
 {
@@ -9,6 +11,16 @@ namespace ZoneReader.LineUpClasses
         public string Purpose { get; set; }
         public List<int> LineUpIds { get; set; }
         public Boundaries Boundaries { get; set; }
+
+        /// <summary>
+        /// Getter for Center vector. Used for displaying targets in webapp.
+        /// </summary>
+        public Vector2 Center { get
+            {
+                return new Vector2((float)Boundaries.GroundPolygon.Centroid.Coordinate.X, (float)Boundaries.GroundPolygon.Centroid.Coordinate.Y);
+            }
+        } 
+
         public static Target NullTargetWithNegativeId => new Target {Id = -1};
     }
 }
